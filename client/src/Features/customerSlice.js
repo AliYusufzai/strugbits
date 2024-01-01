69e2ccfe0a7e2af7b8122139edb09990c3597fc2
Ali Raza Yusufzai <aliyusufzai03@gmail.com>
2024-01-01 18:11:14 +0500

    .

--- client/src/Features/customerSlice.js
+++ client/src/Features/customerSlice.js
@@ -1,18 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const customerSlice = createSlice({
-  name: "customer",
-  initialState: {
-    data: []
-  },
-  reducers: {
-    setCustomers: (state, action) => {
-      state.data = action.payload;
-    }
-  }
+    name: "customer",
+    initialState: {
+        data: [],
+    },
+    reducers: {
+        setCustomers: (state, action) => {
+            state.data = action.payload;
+        },
+        addCustomer: (state, action) => {
+            state.data.push(action.payload);
+        },
+        deleteCustomer: (state, action) => {
+            state.data = state.data.filter(
+                (customer) => customer.id !== action.payload
+            );
+        },
+        setSelectedCustomerId: (state, action) => {
+            state.selectedCustomerId = action.payload;
+        },
+    },
 });
 
-export const { setCustomers } = customerSlice.actions;
+export const {
+    setCustomers,
+    addCustomer,
+    deleteCustomer,
+    setSelectedCustomerId,
+} = customerSlice.actions;
 export const selectCustomers = (state) => state.customer.data;
+export const selectCustomersId = (state) => state.customer.selectedCustomerId;
+
 
 export default customerSlice.reducer;
--- client/src/utility-component/DeleteCustomer.jsx
+++ client/src/utility-component/DeleteCustomer.jsx
@@ -2,51 +2,63 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import { IoTrashOutline } from "react-icons/io5";
 import { GetFile } from "../Services/fileService";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    deleteCustomer,
+    setSelectedCustomerId,
+    selectCustomersId,
+} from "../Features/customerSlice";
 
 export default function DeleteCustomer({
-  showDeleteCustomer,
-  setShowDeleteCustomer,
-  customerId
+    showDeleteCustomer,
+    setShowDeleteCustomer,
 }) {
-  const handleClose = () => setShowDeleteCustomer(false);
-  const { destroyCustomer } = GetFile();
+    const dispatch = useDispatch();
+    const selectedCustomerId = useSelector(selectCustomersId);
+    const handleClose = () => setShowDeleteCustomer(false);
+    const { destroyCustomer } = GetFile();
 
-  const handleDelete = async () => {
-    const response = destroyCustomer(customerId);
-    setShowDeleteCustomer(false);
-  };
-  return (
-    <>
-      <Modal show={showDeleteCustomer} onHide={handleClose} centered>
-        <Modal.Body className="p-5">
-          <div className="text-center">
-            <div className="flex justify-center text-red-500">
-              <IoTrashOutline className="text-8xl mb-4 text-[#D80000]" />
-            </div>
-            <div className="mb-4">
-              <p className="font-semibold text-lg">Are You Sure?</p>
-              <p className="p-3">
-                Do you really want to delete this customer? <br /> This process
-                can not be undone.
-              </p>
-            </div>
-            <div className="flex justify-center space-x-8">
-              <Button
-                className="bg-[#A5A5AF] text-gray-700 px-20 py-2 rounded border-[#A5A5AF] hover:bg-[#A5A5AF]"
-                onClick={handleClose}
-              >
-                Cancel
-              </Button>
-              <Button
-                onClick={handleDelete}
-                className="bg-[#D80000] text-white px-20 py-2 mr-2 rounded border-[#D80000] hover:bg-[#D80000]"
-              >
-                Delete
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
-    </>
-  );
+    const handleDelete = async () => {
+        const response = await destroyCustomer(selectedCustomerId);
+        dispatch(deleteCustomer(selectedCustomerId));
+
+        dispatch(setSelectedCustomerId(null));
+        setShowDeleteCustomer(false);
+    };
+    return (
+        <>
+            <Modal show={showDeleteCustomer} onHide={handleClose} centered>
+                <Modal.Body className="p-5">
+                    <div className="text-center">
+                        <div className="flex justify-center text-red-500">
+                            <IoTrashOutline className="text-8xl mb-4 text-[#D80000]" />
+                        </div>
+                        <div className="mb-4">
+                            <p className="font-semibold text-lg">
+                                Are You Sure?
+                            </p>
+                            <p className="p-3">
+                                Do you really want to delete this customer?{" "}
+                                <br /> This process can not be undone.
+                            </p>
+                        </div>
+                        <div className="flex justify-center space-x-8">
+                            <Button
+                                className="bg-[#A5A5AF] text-gray-700 px-20 py-2 rounded border-[#A5A5AF] hover:bg-[#A5A5AF]"
+                                onClick={handleClose}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                onClick={handleDelete}
+                                className="bg-[#D80000] text-white px-20 py-2 mr-2 rounded border-[#D80000] hover:bg-[#D80000]"
+                            >
+                                Delete
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
 }
